de1bd2c038ade6ecd5650e28a4419a9f73eecb79
baiyalong <baiyalong@gmail.com>
2016-06-03 23:34:58 +0800

    init

---
+++ debug.sh
@@ -0,0 +1,2 @@
+ROOT_URL=http://localhost:3000 PORT=3000 MONGO_URL=mongodb://127.0.0.1:27017/template meteor run -p 3000
+
---
+++ release.sh
@@ -0,0 +1,2 @@
+ROOT_URL=http://localhost:4000 PORT=4000 MONGO_URL=mongodb://127.0.0.1:27017/template meteor run -p 4000   --production
+
